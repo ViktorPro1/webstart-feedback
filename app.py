6c4ba227f0d7e581cf380fedb6348c25169887c4
Viktor Pro <146160822+ViktorPro1@users.noreply.github.com>
2025-05-12 12:53:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,29 +13,29 @@ EMAIL_TO = "webstartstudio978@gmail.com"
 
 @app.route('/send-feedback', methods=['POST'])
 def send_feedback():
-    data = request.json
-    name = data.get('name')
-    viber = data.get('viber')
-    email = data.get('email')
-    telegram = data.get('telegram')
-    message = data.get('message')
-
-    subject = f"Зворотній зв'язок від {name}"
-    body = f"""
-    Ім'я: {name}
-    Вайбер: {viber}
-    Email: {email}
-    Telegram: {telegram}
-    Повідомлення: {message}
-    """
-
-    msg = MIMEMultipart()
-    msg['From'] = EMAIL_USER
-    msg['To'] = EMAIL_TO
-    msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
-
     try:
+        data = request.get_json()  # отримуємо JSON дані з тіла запиту
+        name = data.get('name')
+        viber = data.get('viber')
+        email = data.get('email')
+        telegram = data.get('telegram')
+        message = data.get('message')
+
+        subject = f"Зворотній зв'язок від {name}"
+        body = f"""
+        Ім'я: {name}
+        Вайбер: {viber}
+        Email: {email}
+        Telegram: {telegram}
+        Повідомлення: {message}
+        """
+
+        msg = MIMEMultipart()
+        msg['From'] = EMAIL_USER
+        msg['To'] = EMAIL_TO
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'plain'))
+
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.login(EMAIL_USER, EMAIL_PASS)
         server.send_message(msg)
@@ -43,6 +43,7 @@ def send_feedback():
         return jsonify({"success": True, "message": "Повідомлення надіслано."})
     except Exception as e:
         return jsonify({"success": False, "error": str(e)}), 500
+
 
 if __name__ == "__main__":
     import os
